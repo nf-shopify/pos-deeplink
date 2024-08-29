cf9ba9c6c0562619bde37a1f096270a4f177c023
Neilson Flemming <Neilson.Flemming@Shopify.com>
2024-08-28 22:59:52 -0400

    updated file structure

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const PORT = process.env.PORT || 8000;
 /*----- Middleware Pipe ------*/
 app.use(cors());
 app.use(logger("dev"));
+app.use(express.static('public'))
 
 /*----- Routes ------*/
 app.get("/", (req, res) => {
